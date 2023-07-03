5405c72c225897772b973aa43b4760dc7c625eeb
MACK FISCHER <u1323831@umail.utah.edu>
2023-07-03 07:44:21 -0600

    Add project files.

---
+++ Wordle/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Wordle;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Wordle/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Wordle;
+
+public partial class MainPage : ContentPage
+{
+	private bool isHome;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnStartClicked(object sender, EventArgs e)
+	{
+		if (isHome)
+		{
+            Home.IsEnabled = false;
+            Home.IsVisible = false;
+            Game.IsEnabled = true;
+            Game.IsVisible = true;
+			isHome = false;
+			NavButton.Text = "Home";
+        }
+		else
+		{
+			Home.IsEnabled = true;
+			Home.IsVisible = true;
+            Game.IsEnabled = false;
+            Game.IsVisible = false;
+			isHome = true;
+			NavButton.Text = "Wordle";
+        }
+		
+	}
+}
+
